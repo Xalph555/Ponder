77c70414a103e994a70607aa363284a0cd226386
Sean Lee <81959287+Xalph555@users.noreply.github.com>
2022-03-18 23:58:29 +1100

    Moved grapple pull code to fishing rod (PAC)

--- Common/Player/UsableItems/FishingRod/Hook/Hook.gd
+++ Common/Player/UsableItems/FishingRod/Hook/Hook.gd
@@ -3,68 +3,32 @@
 #--------------------------------------#
 extends KinematicBody2D
 
+class_name Hook
+
 
 # Variables:
 #---------------------------------------
 var _move_speed := 1000
 
-var _pull_x := 60
-var _pull_y := 105
-var _hook_pull := Vector2(_pull_x, _pull_y)
-
-var tension_vec := Vector2.ZERO
-
-var _hook_origin : Position2D
-
 var _move_dir := Vector2.ZERO
 var _hook_dir := Vector2.ZERO
 var _velocity := Vector2.ZERO
 
-var _distance_to_origin := 0.0
-
-var _is_hooked := false
-
-var _parent: KinematicBody2D
+var is_hooked := false
 
 
 # Functions:
 #---------------------------------------
 func _physics_process(delta: float) -> void:
-	_distance_to_origin = self.global_position.distance_to(_hook_origin.global_position)
-	_hook_dir = (self.global_position - _hook_origin.global_position).normalized()
-	
+
 #	display_chain()
 	
-	if _is_hooked:
-		apply_tension()
-	
-	else:
-		fly_hook(delta)
+#	if _is_hooked:
+#		apply_tension()
+#
+#	else:
 
-
-#func display_chain():
-#	# chain graphics
-#	_hook_chain.region_rect.size.x = _distance_to_parent
-#	_hook_chain.rotation = self.position.angle_to_point(_parent.global_position) - self.rotation + deg2rad(180)
-
-
-func apply_tension():
-	_parent.is_flying = true
-	
-	# adjusting pull direction
-	_hook_pull.x = sign(_hook_dir.x) * abs(_hook_pull.x)
-	_hook_pull.y = sign(_hook_dir.y) * abs(_hook_pull.y)
-	
-	tension_vec = _hook_dir * _hook_dir.dot(_hook_pull)
-	
-	_parent.limit_speed = _parent.max_speed * 1.3
-	_parent.velocity += tension_vec
-	
-	# dead space
-	var dead_space := 20.0
-	if _distance_to_origin < dead_space:
-		var parent_origin_dist = (_parent.global_position - _hook_origin.global_position).length()
-		_parent.global_position = self.global_position + (_hook_dir * -1 * (dead_space + parent_origin_dist))
+	fly_hook(delta)
 
 
 func fly_hook(delta: float):
@@ -73,20 +37,18 @@ func fly_hook(delta: float):
 	var collided = move_and_collide(_velocity)
 	
 	if collided:
-		_is_hooked = true
+		is_hooked = true
 		_move_dir = Vector2.ZERO
 		
 #		self.rotation = collided.normal.angle() + deg2rad(180)
 #		display_chain()
 
 
-func shoot(shooter: KinematicBody2D, pos : Position2D, dir: Vector2) -> void:
-	_parent = shooter
-	_hook_origin = pos
-	global_position = _hook_origin.global_position
+func shoot(shooter: KinematicBody2D, pos : Vector2, dir: Vector2) -> void:
+	global_position = pos
 	_move_dir = dir
 	
-	_is_hooked = false
+	is_hooked = false
 
 
 func release() -> void:
--- Common/Player/UsableItems/FishingRod/PACRod/PACFishingRod.gd
+++ Common/Player/UsableItems/FishingRod/PACRod/PACFishingRod.gd
@@ -10,9 +10,18 @@ export(PackedScene) var hook
 
 export var line_length := 20.0
 
-var _hook_instance
+# hook tension variables 
+var _pull_x := 60
+var _pull_y := 105
+var _hook_pull := Vector2(_pull_x, _pull_y)
 
+var tension_vec := Vector2.ZERO
+
+var _hook_instance : Hook
 var _can_grapple := true
+
+var _distance_to_hook := 0.0
+var _hook_dir := Vector2.ZERO
 
 onready var _pivot_point := $PivotPoint
 onready var _hook_point := $PivotPoint/HookPoint
@@ -28,6 +37,18 @@ func _physics_process(delta: float) -> void:
 #		if _can_grapple:
 #			parent.is_flying = false
 	
+	update_input(delta)
+	update_sprite()
+	
+	if _hook_instance:
+		_distance_to_hook = _hook_instance.global_position.distance_to(_hook_point.global_position)
+		_hook_dir = (_hook_instance.global_position - _hook_point.global_position).normalized()
+	
+		if _hook_instance.is_hooked:
+			apply_tension()
+
+
+func update_input(delta: float) -> void:
 	# charging rod
 	if Input.is_action_just_pressed("Action1") and _can_grapple:
 #		_tween.interpolate_property(_pivot_point, "rotation_degrees",
@@ -42,17 +63,15 @@ func _physics_process(delta: float) -> void:
 	# throw rod
 	if Input.is_action_just_released("Action1"):
 		release_grapple()
-	
-	update_sprite()
 
 
 func throw_grapple() -> void:
 	if !_hook_instance:
 		_can_grapple = false
 		
-		var hook_dir : Vector2 = get_global_mouse_position() - _hook_point.global_position
+		var _mouse_dir = get_global_mouse_position() - _hook_point.global_position
 #		print(rad2deg(hook_dir.angle()))
-		hook_dir = hook_dir.normalized()
+		_mouse_dir = _mouse_dir.normalized()
 		
 		#print(rad2deg(to_local(hook_dir).angle()))
 		
@@ -60,11 +79,10 @@ func throw_grapple() -> void:
 		# need to restrict hook direction to flat with hook and 75 degrees down it
 		
 		
-		
 		_hook_instance = hook.instance()
 		_hook_instance.set_as_toplevel(true)
 		add_child(_hook_instance)
-		_hook_instance.shoot(parent, _hook_point, hook_dir)
+		_hook_instance.shoot(parent, _hook_point.global_position, _mouse_dir)
 
 
 func release_grapple() -> void:
@@ -73,6 +91,25 @@ func release_grapple() -> void:
 		
 		_hook_instance.release()
 		_hook_instance = null
+
+
+func apply_tension():
+	parent.is_flying = true
+	
+	# adjusting pull direction
+	_hook_pull.x = sign(_hook_dir.x) * abs(_hook_pull.x)
+	_hook_pull.y = sign(_hook_dir.y) * abs(_hook_pull.y)
+	
+	tension_vec = _hook_dir * _hook_dir.dot(_hook_pull)
+	
+	parent.limit_speed = parent.max_speed * 1.3
+	parent.velocity += tension_vec
+	
+	# dead space
+	var dead_space := 20.0
+	if _distance_to_hook < dead_space:
+		var parent_origin_dist = (parent.global_position - _hook_point.global_position).length()
+		parent.global_position = _hook_instance.global_position + (_hook_dir * -1 * (dead_space + parent_origin_dist))
 
 
 func update_sprite() -> void:
